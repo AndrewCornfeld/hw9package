a96cab7623ddba0d08791249439b359f404ae9bf
Andrew Cornfeld <cpm6gh@virginia.edu>
2024-07-02 11:54:33 -0400

    adding required files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'HW9AndrewCornfeldPackage',
+    author = 'Andrew Cornfeld',
+    author_email = 'cpm6gh@virginia.edu',
+    packages = ['andrewspackage'],
+    url = 'https://github.com/AndrewCornfeld/exercise-9-package',
+    license = 'LICENSE.txt',
+    description = 'andrews cool package',
+)
